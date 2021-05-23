66d2cef8c20c89e71a6fbbf0d2297309e9eb4830
asaadmosa <asaad.mosa@gmail.com>
2021-05-23 15:57:51 +0300

    ex 1

---
+++ Day 12/ex1-js/main.js
@@ -0,0 +1,29 @@
+
+function calculator(a,b){
+    this.a=a;
+    this.b=b;
+}
+
+calculator.prototype.add = function () {
+    return this.a + this.b;
+};
+calculator.prototype.sub= function (){
+    return this.a - this.b;
+}
+
+calculator.prototype.mult= function (){
+    return this.a * this.b;
+}
+
+calculator.prototype.log= function (){
+    console.log('a+b = '+this.add() + ' a-b=' + this.sub() + ' a*b = '+ this.mult());
+}
+
+var c1=new calculator(5,10);
+var c2=new calculator(8,11);
+var c3=new calculator(6,11);
+
+c1.log();
+c2.log();
+
+setTimeout(c3.log,2000);
--- main.js
+++
@@ -1,6 +0,0 @@
-console.log('hello world');
-
-var i=20;
-var j=10;
-
-console.log(i+ '+' + j +'=' + (i+j));
